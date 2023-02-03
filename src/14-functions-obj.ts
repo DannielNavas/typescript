5f93396b7998c732e27a15ac25e7a1d4299b5e45
Danniel Navas <dannielnavas@gmail.com>
2023-02-03 07:03:13 -0500

    ✨ functions obj

---
+++ src/14-functions-obj.ts
@@ -0,0 +1,13 @@
+(() => {
+  //TODO: Function with object as parameter
+
+  const login = (data: { email: string; password: string }): string => {
+    console.log(data.email, data.password);
+  };
+
+  // login('danniel.gmail.com', '123456');
+  login({
+    email: 'danniel.gmail.com',
+    password: '123456',
+  });
+})();
